efadfea7af398d02d888c7a634692e0fee76058b
Koulinn <drdverzola@gmail.com>
2021-10-01 10:25:20 +0100

    moved comments below

--- src/OAuth/OAuth_Strategies/strategy-config.js
+++ src/OAuth/OAuth_Strategies/strategy-config.js
@@ -5,26 +5,76 @@ import User from '../../db/models/User.js'
 import { JWTAuthenticate } from '../jwt-aux.js'
 
 
-// const googleStrategyConfig = {
-//     clientID: process.env.GOOGLE_API_OAUTH_ID,
-//     clientSecret: process.env.GOOGLE_API_SECRET_KEY,
-//     callbackURL: process.env.BE_URL + process.env.PORT + '/user/googleRedirect',
-// }
 const facebookStrategyConfig = {
-    clientID: process.env.FACEBOOK_API_OAUTH_ID,
-    clientSecret: process.env.FACEBOOK_API_SECRET_KEY,
-    callbackURL: process.env.BE_URL + process.env.PORT + '/user/facebookRedirect',
+  clientID: process.env.FACEBOOK_API_OAUTH_ID,
+  clientSecret: process.env.FACEBOOK_API_SECRET_KEY,
+  callbackURL: process.env.BE_URL + process.env.PORT + '/user/facebookRedirect',
 }
 
-// export const googleStrategy = new GoogleStrategy(
-//     googleStrategyConfig
-// ,
-//     async (accessToken, refreshToken, profile, passportNext) => {
-//         try {
-//           console.log(profile)
-    
+export const facebookStrategy = new FacebookStrategy(
+  facebookStrategyConfig
+  ,
+  async (accessToken, refreshToken, profile, passportNext) => {
+    try {
+      console.log(profile)
+
+      const user = await User.findOne({ facebook_Id: profile.id })
+
+      if (user) {
+        const tokens = await JWTAuthenticate(user)
+        passportNext(null, { tokens })
+
+      } else {
+        const newUser = {
+          name: profile.name.givenName,
+          surname: profile.name.familyName,
+          email: profile.emails[0].value,
+          role: "guest",
+          facebook_Id: profile.id,
+        }
+
+        const createdUser = new User(newUser)
+        const savedUser = await createdUser.save()
+        const tokens = await JWTAuthenticate(savedUser)
+
+        passportNext(null, { user: savedUser, tokens })
+      }
+    } catch (error) {
+      console.log(error)
+      passportNext(error)
+    }
+  }
+
+
+)
+
+
+passport.serializeUser(function (user, passportNext) {
+  passportNext(null, user)
+})
+
+export default facebookStrategy
+
+
+
+
+  // const googleStrategyConfig = {
+    //     clientID: process.env.GOOGLE_API_OAUTH_ID,
+    //     clientSecret: process.env.GOOGLE_API_SECRET_KEY,
+    //     callbackURL: process.env.BE_URL + process.env.PORT + '/user/googleRedirect',
+    // }
+
+
+
+  // export const googleStrategy = new GoogleStrategy(
+    //     googleStrategyConfig
+    // ,
+    //     async (accessToken, refreshToken, profile, passportNext) => {
+      //         try {
+        //           console.log(profile)
+
 //           const user = await User.findOne({ googleId: profile.id })
-    
+
 //           if (user) {
 //             const tokens = await JWTAuthenticate(user)
 //             passportNext(null, { tokens })
@@ -37,11 +87,11 @@ const facebookStrategyConfig = {
 //               role: "User",
 //               googleId: profile.id,
 //             }
-    
+
 //             const createdUser = new User(newUser)
 //             const savedUser = await createdUser.save()
 //             const tokens = await JWTAuthenticate(savedUser)
-    
+
 //             passportNext(null, { user: savedUser, tokens })
 //           }
 //         } catch (error) {
@@ -49,49 +99,6 @@ const facebookStrategyConfig = {
 //           passportNext(error)
 //         }
 //       }
-    
-    
+
+
 // )
-export const facebookStrategy = new FacebookStrategy(
-  facebookStrategyConfig
-,
-    async (accessToken, refreshToken, profile, passportNext) => {
-        try {
-          console.log(profile)
-    
-          const user = await User.findOne({ facebook_Id: profile.id })
-    
-          if (user) {
-            const tokens = await JWTAuthenticate(user)
-            passportNext(null, { tokens })
-
-          } else {
-            const newUser = {
-              name: profile.name.givenName,
-              surname: profile.name.familyName,
-              email: profile.emails[0].value,
-              role: "guest",
-              facebook_Id: profile.id,
-            }
-    
-            const createdUser = new User(newUser)
-            const savedUser = await createdUser.save()
-            const tokens = await JWTAuthenticate(savedUser)
-    
-            passportNext(null, { user: savedUser, tokens })
-          }
-        } catch (error) {
-          console.log(error)
-          passportNext(error)
-        }
-      }
-    
-    
-)
-
-
-passport.serializeUser(function (user, passportNext) {
-  passportNext(null, user)
-})
-
-export default googleStrategy
