b469ab03959556382784e29b08feb96068c08591
Koulinn <drdverzola@gmail.com>
2021-10-01 09:37:01 +0100

    added double refresh token

---
+++ src/OAuth/jwt-middle.js
@@ -0,0 +1,33 @@
+import createHttpError from "http-errors"
+import { verifyJWT } from "./jwt-aux.js"
+import User from "../db/models/User.js"
+
+export const JWTAuthMiddleware = async (req, res, next) => {
+
+  if (!req.headers.authorization) {
+    next(createHttpError(401, "Please provide credentials in Authorization header!"))
+  } else {
+    try {
+      
+      const token = req.headers.authorization.replace("Bearer ", "")
+      console.log(token)
+
+      const decodedToken = await verifyJWT(token)
+      console.log(decodedToken, 'decoded token')
+
+      const user = await User.findById(decodedToken._id)
+    
+
+      if (user) {
+        // user
+        req.user = user
+        
+        next()
+      } else {
+        next(createHttpError(404, "User not found!"))
+      }
+    } catch (error) {
+      next(createHttpError(401, "Token not valid!"))
+    }
+  }
+}
