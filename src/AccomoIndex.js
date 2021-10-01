841b16fab82c62df8567c1578ec07e54c5151e1a
Sarath-97 <sarath559709@gmail.com>
2021-10-01 10:28:46 +0100

    initial commit

---
+++ src/AccomoIndex.js
@@ -0,0 +1,25 @@
+//folder to create the endpoints and handlers
+
+import express from "express"
+
+const AccomoRouter = express.Router()
+
+AccomoRouter.post("/register", async (req, res,next) => {
+    
+})
+
+AccomoRouter.get("/register", async (req, res,next) => {
+    
+})
+
+AccomoRouter.get("/register", async (req, res,next) => {
+    
+})
+
+AccomoRouter.put("/register", async (req, res,next) => {
+    
+})
+
+AccomoRouter.post("/register", async (req, res,next) => {
+    
+})
