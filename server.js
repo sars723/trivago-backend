058396a01ab22aa022498928f515cb43b728fba5
sars723 <sarasalomonn@gmail.com>
2021-10-01 11:28:28 +0200

    user crud

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 import express from "express"
 import cors from "cors"
 // import blogPostRouter from "./src/services/blogPost/index.js" -<<<< for accommodation please use the same exact line
-// import usersRouter from "./src/services/user/index.js" -<<<< for user routes same exact line
+import usersRouter from "./src/services/users/index.js" // for user routes same exact line
 import lib from "./src/library/index.js"
 import mongoose from 'mongoose'
 // import passport from "passport"
 // import { googleStrategy } from "./src/auth/Oauth/strategy-config.js"
 
-const {errorHandlers, serverConfig} = lib
+const { errorHandlers, serverConfig } = lib
 
 
 
@@ -21,7 +21,7 @@ server.use(cors(serverConfig))
 // server.use(passport.initialize())
 
 // server.use("/blogPost", blogPostRouter) rename and use for accommodation same exact line
-// server.use("/user", usersRouter) //use for user same exact line
+server.use("/users", usersRouter) //use for user same exact line
 
 
 
@@ -37,18 +37,18 @@ server.use(errorHandlers.server)
 mongoose.connect(process.env.MONGO_CONN)
 
 
-mongoose.connection.on('connected', ()=> {
+mongoose.connection.on('connected', () => {
   console.log('Mongo connected')
   server.listen(PORT, async () => {
     console.log("🚀 Server is running on port ", PORT)
   })
 
-  mongoose.connection.on('error', error =>{
+  mongoose.connection.on('error', error => {
     console.log('Mongo error: ', error)
   })
 
 
-  
+
   server.on("error", (error) =>
     console.log("🚀 Server is crashed due to ", error)
   )
---
+++ src/services/users/index.js
@@ -0,0 +1,80 @@
+import express from "express"
+import UserModel from './schema.js'
+import { adminOnlyMiddleware } from "../../OAuth/admin.js"
+/* import { JWTAuthMiddleware } from "../../OAuth/token.js" */
+
+const usersRouter = express.Router()
+
+usersRouter.post("/register", async (req, res, next) => {
+    try {
+        const newUser = new UserModel(req.body)
+        const { _id } = await newUser.save()
+
+        res.status(201).send({ _id })
+    } catch (error) {
+        next(error)
+    }
+})
+
+usersRouter.get("/", async (req, res, next) => {
+    try {
+        const users = await UserModel.find()
+        res.send(users)
+    } catch (error) {
+        next(error)
+    }
+})
+
+usersRouter.get("/me", async (req, res, next) => {
+    try {
+        /*  res.send(req.user) */
+    } catch (error) {
+        next(error)
+    }
+})
+
+usersRouter.put("/me", async (req, res, next) => {
+    try {
+
+        res.send()
+    } catch (error) {
+        next(error)
+    }
+})
+
+usersRouter.delete("/", async (req, res, next) => {
+    try {
+        /*  await req.user.deleteOne() */
+        res.send()
+    } catch (error) {
+        next(error)
+    }
+})
+usersRouter.get("/:userId"/* , adminOnlyMiddleware */, async (req, res, next) => {
+    try {
+        const user = await UserModel.findById(req.params.userId)
+        res.send(user)
+    } catch (error) {
+        next(error)
+    }
+})
+
+usersRouter.post("/login", async (req, res, next) => {
+    try {
+        const { email, password } = req.body
+
+        const user = await UserModel.checkCredentials(email, password)
+
+        if (user) {
+
+            //generate an access token
+        } else {
+
+            //sending an error (401)
+        }
+    } catch (error) {
+        next(error)
+    }
+})
+
+export default usersRouter
