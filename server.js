146401325646bfc616bb2f67fef737c8d5eabb9a
Sarath-97 <sarath559709@gmail.com>
2021-10-01 11:26:05 +0100

    minor fixes

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import AccomoRouter from "./src/services/accommodation/AccomoIndex.js" //-<<<< f
 import usersRouter from "./src/services/users/index.js" // for user routes same exact line
 import lib from "./src/library/index.js"
 import mongoose from 'mongoose'
+import listEndpoints from "express-list-endpoints"
 // import passport from "passport"
 // import { googleStrategy } from "./src/auth/Oauth/strategy-config.js"
 
@@ -43,6 +44,8 @@ mongoose.connection.on('connected', () => {
   server.listen(PORT, async () => {
     console.log("🚀 Server is running on port ", PORT)
   })
+
+    console.table(listEndpoints(server))
 
   mongoose.connection.on('error', error => {
     console.log('Mongo error: ', error)
