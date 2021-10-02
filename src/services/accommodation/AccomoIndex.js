78777b43718afe1939d465aa03cd0b36c11e2832
Koulinn <drdverzola@gmail.com>
2021-10-02 07:05:36 +0100

    fixed post route

--- src/services/accommodation/AccomoIndex.js
+++ src/services/accommodation/AccomoIndex.js
@@ -4,41 +4,58 @@ import AccomoModel from "./AccomoSchema.js"
 import {JWTAuthMiddleware} from "../../OAuth/jwt-middle.js"
 import { onlyHostAllowedRoute } from "../../OAuth/host_validation_middlew.js"
 import createError from "http-errors"
+import { validationResult } from "express-validator"
+import {accommoValidation} from './validation.js'
 
 
 
 const AccomoRouter = express.Router()
 
 
+// GET /user/me/accommodation
+
+// returns the full list of managed accommodation
+
+
+// will add a new accommodation
+// 400 if invalid data
+// PUT /accommodation/:id
+
+// will edit an existing accommodation
+// 204 ok
+// 404 if not existing
+// DELETE /accommodation/:id
+
+// will delete an existing accommodation
+// 204 if ok
+// 404 if not existing
 
 
 
 
-
-//you have to put the token validation middleware
-//you need to put the host validation middleware
-// and then from req.user you need to retrieve the user._id
-AccomoRouter.post("/", JWTAuthMiddleware, onlyHostAllowedRoute,  async (req, res,next) => {
+ // POST /accommodation
+AccomoRouter.post("/", JWTAuthMiddleware, onlyHostAllowedRoute, accommoValidation,  async (req, res,next) => {
     try {
-      // console.log('inside post accomoo', req.user)
-
-      // for this one you need to merge the body (with the accomodation data)
-      // and retrieve the user ID from req.user._id
-      const userID = req.user._id
-      console.log(req.user._id, 'just ID')
-      // console.log(userId , 'USER ID')
-      const mergedData = {
-        ...req.body,
-        host: userID
+      const errorsList = validationResult(req)
+      if (!errorsList.isEmpty()) {
+        next(createError(400, { errorsList }))
+      } else {
+        
+        const userID = req.user._id
+        console.log(req.user._id, 'just ID')
+        // console.log(userId , 'USER ID')
+        const mergedData = {
+          ...req.body,
+          host: userID
+        }
+        
+         const newAccomo= new AccomoModel(mergedData)
+  
+          const savedAccomo= await newAccomo.save()
+          console.log(savedAccomo)
+      
+          res.status(201).send(savedAccomo)
       }
-      // console.log(mergedData, 'MERGED DATA')
-      //and send this merged objt to the AccomoSchema
-       const newAccomo= new AccomoModel(mergedData)
-
-        const savedAccomo= await newAccomo.save()
-        console.log(savedAccomo)
-    
-        res.status(201).send(savedAccomo)
       } catch (error) {
         next(createError(400, `Invalid Id: ${error}}!`))
       }
